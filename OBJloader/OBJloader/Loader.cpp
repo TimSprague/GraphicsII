91388e1773271df076f450465cf7e9175076e4b8
Timothy Sprague <Tjsprague@fullsail.edu>
2016-04-10 00:19:44 -0400

    Adjusted, and moved back to project. Ignore from here out most likely.

--- OBJloader/OBJloader/Loader.cpp
+++ OBJloader/OBJloader/Loader.cpp
@@ -1,9 +1,13 @@
+// http://www.opengl-tutorial.org/beginners-tutorials/tutorial-7-model-loading/
+
 #include "Loader.h"
 
-bool loadOBJ(const char *path, XMVECTOR & out_verticies, XMVECTOR & out_uvs, XMVECTOR & out_normals)
+
+
+bool loadOBJ(const char *path, vector<Vector> & out_verticies, vector<Vector> & out_uvs, vector<Vector> & out_normals)
 {
 	vector<unsigned int> vertexIndices, uvIndices, normalIndices;
-	vector<Vector> temp_vertices, temp_uvs, temp_normals;
+	vector<Vector> temp_vertices, temp_normals, temp_uvs;
 
 	FILE *file = fopen(path, "r");
 	if (file == nullptr)
@@ -25,19 +29,19 @@ bool loadOBJ(const char *path, XMVECTOR & out_verticies, XMVECTOR & out_uvs, XMV
 		if (strcmp(lineHeader, "v") == 0)
 		{
 			Vector vertex;
-			fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+			fscanf(file, "%f %f %f\n", &vertex.pos.x, &vertex.pos.y, &vertex.pos.z);
 			temp_vertices.push_back(vertex);
 		}
 		else if (strcmp(lineHeader, "vt") == 0)
 		{
 			Vector uv;
-			fscanf(file, "%f %f\n", &uv.x, &uv.y);
+			fscanf(file, "%f %f\n", &uv.pos.x, &uv.pos.y);
 			temp_uvs.push_back(uv);
 		}
 		else if (strcmp(lineHeader, "vn") == 0)
 		{
 			Vector normal;
-			fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+			fscanf(file, "%f %f %f\n", &normal.pos.x, &normal.pos.y, &normal.pos.z);
 			temp_normals.push_back(normal);
 		}
 		else if (strcmp(lineHeader, "f") == 0)
@@ -58,6 +62,26 @@ bool loadOBJ(const char *path, XMVECTOR & out_verticies, XMVECTOR & out_uvs, XMV
 			normalIndices.push_back(normalIndex[1]);
 			normalIndices.push_back(normalIndex[2]);
 		}
+
+		for (unsigned int i = 0; i < vertexIndices.size(); i++)
+		{
+			unsigned int VertexIndex = vertexIndices[i];
+			Vector tempVertex = temp_vertices[VertexIndex - 1];
+			out_verticies.push_back(tempVertex);
+		}
 	}
 
+	ofstream bout;
+	bout.open(path, ios_base::binary | ios_base::app);
+	if (bout.is_open())
+	{
+		bout.write((char*)&out_verticies, sizeof(Vector));
+		bout.close;
+	}
+
+}
+
+void main()
+{
+	
 }
--- OBJloader/OBJloader/Loader.h
+++ OBJloader/OBJloader/Loader.h
@@ -1,4 +1,22 @@
 #pragma once
-#include "Defines.h"
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include "SharedDefines.h"
+#include <DirectXMath.h>
+#include <atlbase.h>
+#include <Windows.h>
+#include <fstream>
 
-bool loadOBJ(const char *path, XMVECTOR & out_verticies, XMVECTOR & out_uvs, XMVECTOR & out_normals)
+using namespace DirectX;
+using namespace std;
+
+struct Vector
+{
+	float3 pos;
+};
+
+bool loadOBJ(const char *path, vector<Vector> & out_verticies, vector<Vector> & out_uvs, vector<Vector> & out_normals)
+
